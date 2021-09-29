e238eab0034362061ae925f64e186c4f7ef97faa
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-09-29 14:43:25 -0400

    basics of system configuration

---
+++ testgrid.tahoe-lafs.org/access-control.nix
@@ -0,0 +1,12 @@
+{ ... }: {
+  # Initial empty root password for easy login:
+  users.users.root.initialHashedPassword = "";
+  services.openssh.permitRootLogin = "prohibit-password";
+
+  services.openssh.enable = true;
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    # Replace this by your SSH pubkey!
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGN4VQm3BIQKEFTw6aPrEwNuShf640N+Py2LOKznFCRT exarkun@bottom"
+  ];
+}
--- testgrid.tahoe-lafs.org/configuration.nix
+++ testgrid.tahoe-lafs.org/configuration.nix
@@ -1,7 +1,15 @@
 { ... }: {
   imports = [
-    ./tahoe-lafs-testgrid.nix
-    ./access-control.nix
-  ]
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
 
+    # Run a Tahoe-LAFS grid
+    ./tahoe-lafs-testgrid.nix
+
+    # Configure authn/authz for system administration
+    ./access-control.nix
+
+    # Configure the rest of the system
+    ./system-configuration.nix
+  ]
 }
