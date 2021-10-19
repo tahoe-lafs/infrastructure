810d1eaa55a37ec6bf57ddebce0051356e1f43fe
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-10-19 09:53:13 -0400

    Don't replace it!  Add yours if you want though.

--- testgrid.tahoe-lafs.org/access-control.nix
+++ testgrid.tahoe-lafs.org/access-control.nix
@@ -7,7 +7,13 @@
   services.openssh.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
-    # Replace this by your SSH pubkey!
+    # If you want access to administer the system, add your ssh pubkey to this
+    # list.  Once the updated configuration is deployed, your key will be
+    # accepted for root authentication.
+    #
+    # A good change would be to create actual user accounts with sudo
+    # configuration instead, probably.
+
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGN4VQm3BIQKEFTw6aPrEwNuShf640N+Py2LOKznFCRT exarkun@bottom"
   ];
 }
