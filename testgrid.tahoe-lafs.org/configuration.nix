4750e5a78ed8aff988d0e4a6db66a3a80a8c406f
Florian Sesser <florian@leastauthority.com>
2025-01-09 18:16:06 +0000

    Tidy up configuration.nix
    Order doesn't matter for nix, but it orders for me when I read the code

--- testgrid.tahoe-lafs.org/configuration.nix
+++ testgrid.tahoe-lafs.org/configuration.nix
@@ -6,13 +6,13 @@
     # This was generated while setting up the machine with nixos-infect.
     ./networking.nix
 
-    # Run a Tahoe-LAFS grid
-    ./tahoe-lafs-testgrid.nix
+    # Configure the rest of the system
+    ./system-configuration.nix
 
     # Configure authn/authz for system administration
     ./access-control.nix
 
-    # Configure the rest of the system
-    ./system-configuration.nix
+    # Run a Tahoe-LAFS grid
+    ./tahoe-lafs-testgrid.nix
   ];
 }
