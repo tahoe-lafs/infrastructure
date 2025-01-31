37e6ff81ad2de175acd3a57b19444710b6db9442
Benoit Donneaux <benoit@leastauthority.com>
2025-01-31 15:18:42 +0100

    Drop the users and define only ssh keys for Hetzner provider
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- tf/core/providers.tf
+++ tf/core/providers.tf
@@ -12,3 +12,14 @@ terraform {
 provider "hcloud" {
   token = var.hcloud_token
 }
+
+# Manage ssh authorized keys so Hetzner can use them to provision our resources (e.g.: new VPS)
+resource "hcloud_ssh_key" "ssh_keys" {
+  for_each = {
+    tf-benoit-000619776016  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZtWY7t8HVnaz6bluYsrAlzZC3MZtb8g0nO5L5fCQKR benoit@leastauthority.com"
+    tf-florian-000018054987 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlPneIaRT/mqu13N83ctEftub4O6zAfi6qgzZKerU5o florian@leastauthority.com"
+  }
+
+  name       = each.key
+  public_key = each.value
+}
