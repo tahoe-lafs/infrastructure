e28e4fd46bafd908feaa9a02d0241e3e644687b0
Benoit Donneaux <benoit@leastauthority.com>
2025-05-15 10:20:53 +0200

    Use better key for NS resources
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- tf/core/dns_of-tl-org.tf
+++ tf/core/dns_of-tl-org.tf
@@ -6,11 +6,11 @@ resource "hetznerdns_zone" "of-tl-org" {
 
 # NS records of this sub-zone
 resource "hetznerdns_record" "of-tl-org_ns" {
-  for_each = toset([
-    "hydrogen.ns.hetzner.com.",
-    "oxygen.ns.hetzner.com.",
-    "helium.ns.hetzner.de.",
-  ])
+  for_each = {
+    primary   = "hydrogen.ns.hetzner.com."
+    secondary = "oxygen.ns.hetzner.com."
+    tertiary  = "helium.ns.hetzner.de."
+  }
 
   name    = "@"
   type    = "NS"
