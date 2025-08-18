4f34dc6568c4560b6d3b6ec1972a96fbff22124c
Benoit Donneaux <benoit@leastauthority.com>
2025-08-18 10:41:05 +0200

    Avoid debug output in backup scripts

--- nix/hosts/webforge/backup_postexec.sh
+++ nix/hosts/webforge/backup_postexec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Configure bash behavior
-set -o xtrace   # print every call to help debugging
+#set -o xtrace   # print every call to help debugging
 set -o errexit  # exit on failed command
 set -o nounset  # exit on undeclared variables
 set -o pipefail # exit on any failed command in pipes
